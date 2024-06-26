16bde994bec1d33fe125e620316c71732c8ee2ca
ozkary <ozkary@gmail.com>
2024-06-26 18:09:06 -0400

    add customer page

---
+++ app/nextjs-dashboard/app/ui/customers/create-form.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { CustomerField } from '@/app/lib/definitions';
+import Link from 'next/link';
+import {
+  CheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { useFormState } from 'react-dom';
+
+import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions'
+
+import displayFieldError from '../error-label';
+
+export default function Form() {
+  
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+  
+  return (
+    <form action={dispatch}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Customer Name */}
+        <div className="mb-4">
+          <label htmlFor="name" className="mb-2 block text-sm font-medium">
+            Full Name
+          </label>
+          <div className="relative">
+            <input
+                  id="name"
+                  name="name"
+                  type="text"                  
+                  placeholder="Enter the full name"
+                  className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  aria-describedby="name-error"
+            />            
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="name" aria-live="polite" aria-atomic="true">
+            {displayFieldError(state.errors?.customerId)}
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className="mb-4">
+          <label htmlFor="email" className="mb-2 block text-sm font-medium">
+            Email
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="email"
+                name="email"
+                type="email"                
+                placeholder="Enter an email"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
+              />
+              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="email-error" aria-live="polite" aria-atomic="true">
+              {displayFieldError(state.errors?.email)}
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/invoices"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create Customer</Button>
+      </div>
+    </form>
+  );
+}
